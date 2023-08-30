cd37cb6e26bf25a2bf3eca4a59154535d60974c5
Pedro Correia <correiapc@hotmail.com>
2023-08-30 20:47:07 +0100

    update images

--- srcs/so_long.c
+++ srcs/so_long.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: correia <correia@student.42.fr>            +#+  +:+       +#+        */
+/*   By: pveiga-c <pveiga-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 17:05:55 by pveiga-c          #+#    #+#             */
-/*   Updated: 2023/08/30 08:41:12 by correia          ###   ########.fr       */
+/*   Updated: 2023/08/30 20:24:34 by pveiga-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void	game_init(t_win *so_long)
 {
 	so_long->mlx_ptr = mlx_init();
-	so_long->win_ptr = mlx_new_window(so_long->mlx_ptr, so_long->map->width ,
-			so_long->map->height, "SO LONG");
+	so_long->win_ptr = mlx_new_window(so_long->mlx_ptr, so_long->map->width * 64 ,
+			so_long->map->height * 64, "SO LONG");
 	so_long->img = malloc(sizeof(t_img));
 	images(so_long);
 	draw_imgs(so_long);
@@ -25,7 +25,7 @@ void	game_init(t_win *so_long)
 
 static void	put_img(t_win *so_long, void *img, int x, int y)
 {
-	mlx_put_image_to_window(so_long->mlx_ptr, so_long->win_ptr, img, x, y);
+	mlx_put_image_to_window(so_long->mlx_ptr, so_long->win_ptr, img, x * 64, y * 64);
 }
 
 void	draw_imgs(t_win *so_long)
@@ -42,13 +42,13 @@ void	draw_imgs(t_win *so_long)
 			if (so_long->matrix[i][j] == 'P')
 				put_img(so_long, so_long->img->player, j, i);
 			else if (so_long->matrix[i][j] == 'C')
-				put_img(so_long, so_long->img->player, j, i);
+				put_img(so_long, so_long->img->collectible, j, i);
 			else if (so_long->matrix[i][j] == '1')
-				put_img(so_long, so_long->img->player, j, i);
+				put_img(so_long, so_long->img->wall, j, i);
 			else if (so_long->matrix[i][j] == 'E')
-				put_img(so_long, so_long->img->player, j, i);
+				put_img(so_long, so_long->img->exit, j, i);
 			else if (so_long->matrix[i][j] == '0')
-				put_img(so_long, so_long->img->player, j, i);
+				put_img(so_long, so_long->img->floor, j, i);
 			j++;
 		}
 		i++;
@@ -57,7 +57,15 @@ void	draw_imgs(t_win *so_long)
 
 void	images(t_win *so_long)
 {
-	so_long->img->player = mlx_xpm_file_to_image(so_long->mlx_ptr, "./image/xpm/pato.xpm", &so_long->map->width,
+	so_long->img->player = mlx_xpm_file_to_image(so_long->mlx_ptr, "./image/xpm/floor4.xpm", &so_long->map->width,
+			&so_long->map->height);
+	so_long->img->wall = mlx_xpm_file_to_image(so_long->mlx_ptr, "./image/xpm/wall.xpm", &so_long->map->width,
+			&so_long->map->height);
+	so_long->img->floor = mlx_xpm_file_to_image(so_long->mlx_ptr, "./image/xpm/floor.xpm", &so_long->map->width,
+			&so_long->map->height);	
+	so_long->img->collectible = mlx_xpm_file_to_image(so_long->mlx_ptr, "./image/xpm/floor4.xpm", &so_long->map->width,
+			&so_long->map->height);	
+	so_long->img->exit = mlx_xpm_file_to_image(so_long->mlx_ptr, "./image/xpm/floor4.xpm", &so_long->map->width,
 			&so_long->map->height);
 }
 
